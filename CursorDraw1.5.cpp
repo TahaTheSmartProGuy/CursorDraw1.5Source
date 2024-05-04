2778b06f5fe751ce9e90faa8cb002c867fa0fb41
Taha! <164132872+TahaTheSmartProGuy@users.noreply.github.com>
2024-05-04 11:09:05 +0300

    Add files via upload

---
+++ CursorDraw1.5.cpp
@@ -0,0 +1,414 @@
+﻿#include <windows.h>
+#include <tchar.h>
+#include <ctime>
+#include <iostream>
+#include <sstream>
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib,"Msimg32.lib")
+#include <math.h>
+#include <cmath>
+#include <time.h>
+typedef struct
+{
+	float x;
+	float y;
+	float z;
+} VERTEX;
+
+typedef struct
+{
+	int vtx0;
+	int vtx1;
+} EDGE;
+#define M_PI   3.14159265358979323846264338327950288
+//#define PI   3.14159265358979323846264338327950288
+typedef NTSTATUS(NTAPI* NRHEdef)(NTSTATUS, ULONG, ULONG, PULONG, ULONG, PULONG);
+typedef NTSTATUS(NTAPI* RAPdef)(ULONG, BOOLEAN, BOOLEAN, PBOOLEAN);
+typedef union _RGBQUAD {
+	COLORREF rgb;
+	struct {
+		BYTE b;
+		BYTE g;
+		BYTE r;
+		BYTE Reserved;
+	};
+}_RGBQUAD, * PRGBQUAD;
+typedef struct
+{
+	FLOAT h;
+	FLOAT s;
+	FLOAT l;
+} HSL;
+
+namespace Colors
+{
+	//These HSL functions was made by Wipet, credits to him!
+	//OBS: I used it in 3 payloads
+
+	//Btw ArTicZera created HSV functions, but it sucks unfortunatelly
+	//So I didn't used in this malware.
+
+	HSL rgb2hsl(RGBQUAD rgb)
+	{
+		HSL hsl;
+
+		BYTE r = rgb.rgbRed;
+		BYTE g = rgb.rgbGreen;
+		BYTE b = rgb.rgbBlue;
+
+		FLOAT _r = (FLOAT)r / 255.f;
+		FLOAT _g = (FLOAT)g / 255.f;
+		FLOAT _b = (FLOAT)b / 255.f;
+
+		FLOAT rgbMin = min(min(_r, _g), _b);
+		FLOAT rgbMax = max(max(_r, _g), _b);
+
+		FLOAT fDelta = rgbMax - rgbMin;
+		FLOAT deltaR;
+		FLOAT deltaG;
+		FLOAT deltaB;
+
+		FLOAT h = 0.f;
+		FLOAT s = 0.f;
+		FLOAT l = (FLOAT)((rgbMax + rgbMin) / 2.f);
+
+		if (fDelta != .5f)
+		{
+			s = l < .5f ? (FLOAT)(fDelta / (rgbMax + rgbMin)) : (FLOAT)(fDelta / (2.f - rgbMax - rgbMin));
+			deltaR = (FLOAT)(((rgbMax - _r) / 6.f + (fDelta / 2.f)) / fDelta);
+			deltaG = (FLOAT)(((rgbMax - _g) / 6.f + (fDelta / 2.f)) / fDelta);
+			deltaB = (FLOAT)(((rgbMax - _b) / 6.f + (fDelta / 2.f)) / fDelta);
+
+			if (_r == rgbMax)      h = deltaB - deltaG;
+			else if (_g == rgbMax) h = (1.f / 3.f) + deltaR - deltaB;
+			else if (_b == rgbMax) h = (2.f / 3.f) + deltaG - deltaR;
+			if (h < 0.f)           h += 1.f;
+			if (h > 1.f)           h -= 1.f;
+		}
+
+		hsl.h = h;
+		hsl.s = s;
+		hsl.l = l;
+		return hsl;
+	}
+
+	RGBQUAD hsl2rgb(HSL hsl)
+	{
+		RGBQUAD rgb;
+
+		FLOAT r = hsl.l;
+		FLOAT g = hsl.l;
+		FLOAT b = hsl.l;
+
+		FLOAT h = hsl.h;
+		FLOAT sl = hsl.s;
+		FLOAT l = hsl.l;
+		FLOAT v = (l <= 0.f) ? (l * (1.f + sl)) : (l + sl - l * sl);
+
+		FLOAT m;
+		FLOAT sv;
+		FLOAT fract;
+		FLOAT vsf;
+		FLOAT mid1;
+		FLOAT mid2;
+
+		INT sextant;
+
+		if (v > 0.f)
+		{
+			m = l + l - v;
+			sv = (v - m) / v;
+			h *= 6.f;
+			sextant = (INT)h;
+			fract = h - sextant;
+			vsf = v * sv * fract;
+			mid1 = m + vsf;
+			mid2 = v - vsf;
+
+			switch (sextant)
+			{
+			case 0:
+				r = v;
+				g = mid1;
+				b = m;
+				break;
+			case 1:
+				r = mid2;
+				g = v;
+				b = m;
+				break;
+			case 2:
+				r = m;
+				g = v;
+				b = mid1;
+				break;
+			case 3:
+				r = m;
+				g = mid2;
+				b = v;
+				break;
+			case 4:
+				r = mid1;
+				g = m;
+				b = v;
+				break;
+			case 5:
+				r = v;
+				g = m;
+				b = mid2;
+				break;
+			case 6:
+				r = v;
+				g = m;
+				b = mid2;
+				break;
+			}
+		}
+
+		rgb.rgbRed = (BYTE)(r * 255.f);
+		rgb.rgbGreen = (BYTE)(g * 255.f);
+		rgb.rgbBlue = (BYTE)(b * 255.f);
+
+		return rgb;
+	}
+}
+int red, green, blue;
+bool ifcolorblue = false, ifblue = false;
+COLORREF Hue(int length) { //Credits to Void_/GetMBR
+	if (red != length) {
+		red < length; red++;
+		if (ifblue == true) {
+			return RGB(red, 0, length);
+		}
+		else {
+			return RGB(red, 0, 0);
+		}
+	}
+	else {
+		if (green != length) {
+			green < length; green++;
+			return RGB(length, green, 0);
+		}
+		else {
+			if (blue != length) {
+				blue < length; blue++;
+				return RGB(0, length, blue);
+			}
+			else {
+				red = 0; green = 0; blue = 0;
+				ifblue = true;
+			}
+		}
+	}
+}
+
+DWORD WINAPI Payload1(LPVOID lpParam) {
+	HDC hdc = GetDC(NULL);
+	HDC hdcCopy = CreateCompatibleDC(hdc);
+	int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+	int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+	BITMAPINFO bmpi = { 0 };
+	HBITMAP bmp;
+
+	bmpi.bmiHeader.biSize = sizeof(bmpi);
+	bmpi.bmiHeader.biWidth = screenWidth;
+	bmpi.bmiHeader.biHeight = screenHeight;
+	bmpi.bmiHeader.biPlanes = 1;
+	bmpi.bmiHeader.biBitCount = 32;
+	bmpi.bmiHeader.biCompression = BI_RGB;
+
+
+	RGBQUAD* rgbquad = NULL;
+	HSL hslcolor;
+
+	bmp = CreateDIBSection(hdc, &bmpi, DIB_RGB_COLORS, (void**)&rgbquad, NULL, 0);
+	SelectObject(hdcCopy, bmp);
+
+	INT i = 0;
+
+	while (1)
+	{
+		hdc = GetDC(NULL);
+		StretchBlt(hdcCopy, 0, 0, screenWidth, screenHeight, hdc, 0, 0, screenWidth, screenHeight, NOTSRCCOPY);
+		StretchBlt(hdcCopy, 0, 0, screenWidth, screenHeight, hdc, 0, 0, screenWidth, screenHeight, SRCCOPY);
+		StretchBlt(hdcCopy, 0, 0, screenWidth, screenHeight, hdc, 0, 0, screenWidth, screenHeight, NOTSRCERASE);
+		StretchBlt(hdcCopy, 0, 0, screenWidth, screenHeight, hdc, 0, 0, screenWidth, screenHeight, SRCERASE);
+		StretchBlt(hdcCopy, 0, 0, screenWidth, screenHeight, hdc, 0, 0, screenWidth, screenHeight, SRCPAINT);
+
+		RGBQUAD rgbquadCopy;
+
+		for (int x = 0; x < screenWidth; x++)
+		{
+			for (int y = 0; y < screenHeight; y++)
+			{
+				int index = y * screenWidth + x;
+				int fx = (int)((i ^ 4) + (i * 4) * cbrt(15));
+
+				rgbquadCopy = rgbquad[index];
+
+				hslcolor = Colors::rgb2hsl(rgbquadCopy);
+				hslcolor.h = fmod(fx / 450.5f + y / screenHeight * 2.f, 1.f);
+
+				rgbquad[index] = Colors::hsl2rgb(hslcolor);
+			}
+		}
+
+		i++;
+		;
+		StretchBlt(hdc, 0, 0, screenWidth, screenHeight, hdcCopy, 0, 0, screenWidth, screenHeight, NOTSRCCOPY);
+		StretchBlt(hdc, 0, 0, screenWidth, screenHeight, hdcCopy, 0, 0, screenWidth, screenHeight, SRCCOPY);
+		StretchBlt(hdc, 0, 0, screenWidth, screenHeight, hdcCopy, 0, 0, screenWidth, screenHeight, NOTSRCERASE);
+		StretchBlt(hdc, 0, 0, screenWidth, screenHeight, hdcCopy, 0, 0, screenWidth, screenHeight, SRCERASE);
+		StretchBlt(hdc, 0, 0, screenWidth, screenHeight, hdcCopy, 0, 0, screenWidth, screenHeight, SRCPAINT);
+
+		ReleaseDC(NULL, hdc);
+		DeleteDC(hdc);
+	}
+
+	return 0x75;
+}
+
+DWORD WINAPI Payload2(LPVOID lpParam) {
+	HDC hdc = GetDC(NULL);
+	HDC hdcCopy = CreateCompatibleDC(hdc);
+	int w = GetSystemMetrics(0);
+	int h = GetSystemMetrics(1);
+	BITMAPINFO bmpi = { 0 };
+	HBITMAP bmp;
+	bmpi.bmiHeader.biSize = sizeof(bmpi);
+	bmpi.bmiHeader.biWidth = w;
+	bmpi.bmiHeader.biHeight = h;
+	bmpi.bmiHeader.biPlanes = 1;
+	bmpi.bmiHeader.biBitCount = 32;
+	bmpi.bmiHeader.biCompression = BI_RGB;
+	RGBQUAD* rgbquad = NULL;
+	HSL hslcolor;
+	bmp = CreateDIBSection(hdc, &bmpi, DIB_RGB_COLORS, (void**)&rgbquad, NULL, 0);
+	SelectObject(hdcCopy, bmp);
+	INT i = 0;
+	while (1)
+	{
+		hdc = GetDC(NULL);
+		StretchBlt(hdcCopy, 0, 0, w, h, hdc, 0, 0, w, h, SRCCOPY);
+		RGBQUAD rgbquadCopy;
+		for (int x = 0; x < w; x++)
+		{
+			for (int y = 0; y < h; y++)
+			{
+				int index = y * w + x;
+				int fx = (int)((i ^ 4) + (i * 4) * cbrt(x * y));
+				rgbquadCopy = rgbquad[index];
+				hslcolor = Colors::rgb2hsl(rgbquadCopy);
+				hslcolor.h = fmod(fx / 300.f + y / h * .1f + i / 1000.f, 1.f);
+				rgbquad[index] = Colors::hsl2rgb(hslcolor);
+			}
+		}
+
+		i++;
+		StretchBlt(hdc, 0, 0, w, h, hdcCopy, 0, 0, w, h, SRCCOPY);
+		ReleaseDC(NULL, hdc);
+		DeleteDC(hdc);
+	}
+	return 0x75;
+}
+
+DWORD WINAPI Payload3(LPVOID lpParam) {
+		int x = GetSystemMetrics(0);
+		int y = GetSystemMetrics(1);
+		LPCSTR text = 0;
+		while (1)
+		{
+			HDC hdc = GetDC(0);
+			SetBkMode(hdc, 0);
+			text = "YOU CANT ESCAPE US!";
+			SetTextColor(hdc, RGB(rand() % 255, rand() % 255, rand() % 255));
+			HFONT font = CreateFontA(45, 35, 0, 0, FW_THIN, 0, 1, 0, ANSI_CHARSET, 0, 0, 0, 0, "Comic Sans MS");
+			SelectObject(hdc, font);
+			TextOutA(hdc, rand() % x, rand() % y, text, strlen(text));
+			Sleep(0);
+			DeleteObject(font);
+			ReleaseDC(0, hdc);
+		}
+	}
+
+VOID WINAPI Sound1() {
+	HWAVEOUT hWaveOut = 0;
+	WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
+	waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+	char buffer[8000 * 30] = {};
+	for (DWORD t = 0; t < sizeof(buffer); ++t)
+		buffer[t] = static_cast<char>(t >> 5) | (t << 4) | ((t & 1023) ^ 1981) | ((t - 67) >> 4);
+
+	WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
+	waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutClose(hWaveOut);
+}
+
+VOID WINAPI Sound2() {
+	HWAVEOUT hWaveOut = 0;
+	WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
+	waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+	char buffer[8000 * 30] = {};
+	for (DWORD t = 0; t < sizeof(buffer); ++t)
+		buffer[t] = static_cast<char>((t * (1 | (t >> 10 - ((t >> 17) % 4)) % 8)) & (8 << ((t >> 13) % 4)) * ((1 | (t >> 15) % 8))) * 11 * (t >> 10);
+
+	WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
+	waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutClose(hWaveOut);
+}
+
+VOID WINAPI Sound3() {
+	HWAVEOUT hWaveOut = 0;
+	WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
+	waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+	char buffer[8000 * 30] = {};
+	for (DWORD t = 0; t < sizeof(buffer); ++t)
+		buffer[t] = static_cast<char>(t >> 5 | (t >> 0) * (t >> 5));
+
+	WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
+	waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutClose(hWaveOut);
+}
+
+
+int main()
+
+{
+	ShowWindow(GetConsoleWindow(), SW_HIDE);
+	if (MessageBoxW(NULL, L"WARNING!!!\r\n\You're About To Run A Malware Named CursorDraw (NoSkid)\r\n\It Doesn't Delete All Your Data", L"CursorDraw (NoSkid)", MB_YESNO | MB_ICONEXCLAMATION) == IDNO)
+	{
+		ExitProcess(0);
+	}
+	else
+	{
+		if (MessageBoxW(NULL, L"F̷i̷n̸a̴l̴ ̴W̸a̴r̸n̷i̸n̷g̷ !!!\r\n\You're About To Run A Malware Named CursorDraw\r\n\it Won't Delete All Your Data\r\n\NOT FOR EPILEPSY", L"F̷i̷n̸a̴l̴ ̴W̸a̴r̸n̷i̸n̷g̷ - CursorDraw (Again, NoSkid)", MB_YESNO | MB_ICONEXCLAMATION) == IDNO)
+		{
+			ExitProcess(0);
+		}
+		else
+		{
+			Sleep(2500);
+			HANDLE thread1 = CreateThread(0, 0, Payload1, 0, 0, 0);
+			Sound1();
+			Sleep(30000);
+			TerminateThread(thread1, 0);
+			CloseHandle(thread1);
+			HANDLE thread2 = CreateThread(0, 0, Payload2, 0, 0, 0);
+			Sound2();
+			Sleep(30000);
+			TerminateThread(thread2, 0);
+			CloseHandle(thread2);
+			HANDLE thread3 = CreateThread(0, 0, Payload3, 0, 0, 0);
+			Sound3();
+			Sleep(30000);
+			TerminateThread(thread3, 0);
+			CloseHandle(thread3);
+		}
+	}
+}
